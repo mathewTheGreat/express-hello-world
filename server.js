473f2238d44f43a82bc6753df6b7eb3f3012ecb0
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 15:06:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -55,11 +55,9 @@ const Test = mongoose.model('Test', new mongoose.Schema({ name: String }));
 //const m = createDocument(Test, newDocument);
 
 
-findAllDocuments(Test).then(res => {
-        console.log(res)
-      })
-      .catch(error => {
-        console.error(error);
-      });
+findAllDocuments(Test).then(
+    value => console.log(value),  // logs 42
+  error => console.log(error)
+)
 
 //console.log(t)
