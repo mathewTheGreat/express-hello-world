7540b8af40a12047589c76ff013f1fdff747ad83
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 14:09:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const collection = db.collection('wallet');
 
     collection.find({}).toArray(function(err, docs) {
   console.log("Found the following records");
-  console.log(docs);
+  console.log(collection);
 });
 
 
