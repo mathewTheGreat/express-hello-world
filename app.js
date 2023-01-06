c041e41923d2e6b0a167ac53eb213cbc026b4c26
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 12:53:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send('Mathew Momanyi'));
+app.use("/", (req, res) => {
+    res.send('welcome to the server home page')
+})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('backend running')
