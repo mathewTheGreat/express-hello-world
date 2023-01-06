13184083fd3e44a3a250b7063353aeaa14e17219
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 12:49:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send('Mathew Momanyi'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+app.listen(process.env.PORT || 5000, () => {
+    console.log('backend running')
+})
 
