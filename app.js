a192d048428cdac258d22b25184e00dac2aff74a
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 12:42:08 +0300

    update

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
-const express = require('express')
-const path = require("path");
-const app = express()
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-// #############################################################################
-// Logs all request paths and method
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
-});
+app.get("/", (req, res) => res.send('Mathew Momanyi'));
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-// #############################################################################
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
-})
 
-module.exports = app
