9e367381ea2a452ac3dc35988a1acdbfd66bf872
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 14:16:21 +0300

    installed mongoose

--- server.js
+++ server.js
@@ -9,25 +9,22 @@ app.listen(process.env.PORT || 5000, () => {
     console.log('backend running')
 })
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://mongo:W8C3r87LZ0mPd2b4e2j2@containers-us-west-126.railway.app:6482";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  //const collection = client.db("wallet").collection("63b7f9f80ea4ce001c42d1bb");
-  // perform actions on the collection object
-    //console.log(collection)
-    const db = client.db('wallet');
-const collection = db.collection('wallet');
-
-    collection.find({}).toArray(function(err, docs) {
-  console.log("Found the following records");
-  console.log(collection);
-});
 
 
 
-
-  client.close();
-});
-
-
+const mongoose = require("mongoose");
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://mongo:W8C3r87LZ0mPd2b4e2j2@containers-us-west-126.railway.app:6482,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
