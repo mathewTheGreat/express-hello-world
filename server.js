5a9f927d5c5cee2a78db49ed840599847361edd3
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 13:52:04 +0300

    up

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.listen(process.env.PORT || 5000, () => {
     console.log('backend running')
 })
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://mongo:W8C3r87LZ0mPd2b4e2j2@containers-us-west-126.railway.app:6482";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("wallet").collection("63b7f9f80ea4ce001c42d1bb");
+  // perform actions on the collection object
+    console.log(collection)
+  client.close();
+});
