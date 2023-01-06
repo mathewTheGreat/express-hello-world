8d95b4df6087cf874411a1dccd2109484799eb75
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 13:00:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send('welcome to the server home page')
+    res.send('Mathew Momanyi')
 })
 
 app.listen(process.env.PORT || 5000, () => {
