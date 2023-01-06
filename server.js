7c7d9981c368c51e71fd6536e5562227c8c1aaea
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-06 14:01:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,25 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb://mongo:W8C3r87LZ0mPd2b4e2j2@containers-us-west-126.railway.app:6482";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("wallet").collection("63b7f9f80ea4ce001c42d1bb");
+  //const collection = client.db("wallet").collection("63b7f9f80ea4ce001c42d1bb");
   // perform actions on the collection object
-    console.log(collection)
+    //console.log(collection)
+    const db = client.db('wallet');
+const collection = db.collection('63b7f9f80ea4ce001c42d1bb');
+
+    collection.find({}).toArray(function(err, docs) {
+  console.log("Found the following records");
+  console.log(docs);
+});
+I hope this helps! Let me know if you have any questions.
+
+
+
+
+
   client.close();
 });
+
+const db = client.db('wallet');
+const collection = db.collection('63b7f9f80ea4ce001c42d1bb');
+
